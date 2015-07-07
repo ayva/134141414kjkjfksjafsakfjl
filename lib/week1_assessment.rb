fe0f1dca7637476cb4043df19e650023e0ca018b
ayva <olga.ayva@gmail.com>
2015-07-07 09:00:44 -0700

    22 done, 2 failures, enemies are not added in an array

--- lib/week1_assessment.rb
+++ lib/week1_assessment.rb
@@ -1,2 +1,67 @@
 # Put your code here to make the tests pass
 
+class Weapon
+  attr_reader :name, :damage
+  
+  def initialize(name="Name", damage)
+    #@weapon
+    @name=name
+    @damage=damage
+
+  end
+
+
+
+end
+
+class BattleBot
+  attr_reader :name, :health, :weapon, :enemies
+
+  def initialize( name = "Name",
+                  health = 100,
+                  weapon = nil,
+                  enemies = [])
+    @name = name
+    @health = health
+    @weapon = weapon 
+    @enemies = enemies
+  end
+
+  def dead?
+    health == 0 ? true : false
+  end
+
+  def pick_up(weapon)
+     @weapon=weapon if @weapon.nil?
+  end
+
+  def drop_weapon
+    @weapon=nil
+  end
+
+  def attack(bot2)
+    raise "No weapon" if @weapon == nil
+    raise ArgumentError unless bot2.class == BattleBot
+    enemies_count(bot2)
+    bot2.take_damage(weapon.damage)
+    bot2.dead?
+    
+  end
+
+  def take_damage(damage)
+    @health-=damage
+  end
+
+  def enemies
+     @enemies
+  end
+
+  def enemies_count(bot)
+    
+      @enemies << bot
+    
+  end 
+
+  
+
+end
